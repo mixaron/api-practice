16cfb38a24bb74c4b996bb7fc95b3e9f97136a80
Anton Ryabchuk <tosha.ryabchuk@gmail.com>
2025-05-29 20:27:42 +0400

    add db connect

---
+++ internal/db/db_connect.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+var DB *gorm.DB
+
+func Init() {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+
+	log.Println("success connect to db")
+}
