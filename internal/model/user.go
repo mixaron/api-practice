2be5f5bbe73e9b82830a4663a024f8291a150fd8
Anton Ryabchuk <tosha.ryabchuk@gmail.com>
2025-05-29 20:29:09 +0400

    add user entity

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,1 +1,9 @@
 package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Email    string `json:"email" gorm:"unique" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
